49bdfc64e8178c8ff3c5c55df9bb3ad7fa7e579f
mblaze1986 <morty.blaze@gmail.com>
2020-10-06 05:22:01 +0300

    form+upd

--- src/js/app.js
+++ src/js/app.js
@@ -24,6 +24,52 @@ $(document).ready(function () {
     $('#popularWrap').toggleClass('active');
   });
 
+  $(document).ready(function () {
+    const btnSelector = '.backform__collapse-button';
+    const targetMenuSelector = '.backform__collapse-list';
+    $(btnSelector).each(function () {
+      $(this).click(function (event) {
+        console.log(this);
+        console.log('click');
+        const $targetElem = $(this).parent().find(targetMenuSelector);
+        $(this).parent().find(targetMenuSelector).toggleClass('hidden').toggleClass('open');
+        $(document).mouseup(function (e) {
+          let container = $targetElem;
+          if (container.has(e.target).length === 0) {
+            container.addClass('hidden');
+          }
+        });
+      });
+    });
+    $('.backform__collapse-clause').each(function () {
+      $(this).click(function (event) {
+        $(event.currentTarget).parents('.backform__input').find(btnSelector).html($(event.currentTarget).html() + '<span>&#9660;</span>');
+        $(event.currentTarget).parents('.backform__input').find(targetMenuSelector).addClass('hidden');
+      });
+    });
+  });
+
+  let modalCall = $("[data-modal]");
+  let modalClose = $("[data-close]");
+  modalCall.on("click", function (event) {
+    event.preventDefault();
+    let modalId = $(this).data('modal');
+
+    $(modalId).addClass('show');
+    $("body").addClass('no-scroll');
+  });
+
+  modalClose.on("click", function (event) {
+    event.preventDefault();
+
+    let modalParent = $(this).parents('.modal');
+
+    modalParent.removeClass('show');
+    $("body").removeClass('no-scroll');
+  });
+
+
+
   $("[data-slider]").slick({
     arrows: false,
     dots: true,
